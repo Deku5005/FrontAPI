e19ec289d4acbbba47bea30f3a133a0d4db5849c
Mamoutou02 <sangaremoutou.360@.com>
2025-08-14 20:34:18 +0000

    bouton se deconnecter chez admin ok,chez contributeur le code ok mes sa ne marche pas

--- src/app/features/components-administrateur/header/header.ts
+++ src/app/features/components-administrateur/header/header.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
-import {RouterOutlet} from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { RouterLink } from '@angular/router'; // Changé de RouterOutlet à RouterLink
 
 @Component({
   selector: 'app-header',
   imports: [
-    FaIconComponent,RouterOutlet
+    FaIconComponent,
+    RouterLink 
   ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-
   protected readonly faUserCircle = faUserCircle;
 }
