0eaa2811d4055849a6c09c1130bf8f90000454ba
Mamoutou02 <sangaremoutou.360@.com>
2025-08-14 10:43:38 +0000

    MAj

--- src/app/features/components-administrateur/projets/projets.ts
+++ src/app/features/components-administrateur/projets/projets.ts
@@ -8,7 +8,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, DatePipe, CommonModule, HttpClientModule],
   templateUrl: './projets.html',
-  styleUrls: ['./projets.css'] 
+  styleUrls: ['./projets.css']
 })
 export class Projets implements OnInit {
 
@@ -17,7 +17,7 @@ export class Projets implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    const contributeurId = 2;  
+    const contributeurId = 2;
     const apiUrl = `http://localhost:8080/api/projets/recupere/${contributeurId}`;
     this.http.get<any[]>(apiUrl).subscribe({
       next: (res) => {
